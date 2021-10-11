9c841ae6a4d5b9981ef527e93fa08261297b86fd
kemallt <kemallt@gmail.com>
2021-10-11 15:20:46 +0300

    refactor convertItem, generateStylish functions;

--- src/Differ.php
+++ src/Differ.php
@@ -21,7 +21,7 @@ function genDiff($pathToFile1, $pathToFile2, $formatter = "stylish")
 
 function createDiffObjects($data1, $data2)
 {
-    $dataArr = convertItem($data1, [], '-');
+    $dataArr = convertItem($data1, ['_sign' => ' ', '_signAdd' => ' '], '-');
     return convertItem($data2, $dataArr, '+');
 }
 
@@ -44,34 +44,44 @@ function convertItem($item, $itemArr, $sign)
     $iter = function ($curItem, $curItemArr) use (&$iter, $sign) {
         if (!is_object($curItem)) {
             $curItemVal = stringifyItem($curItem);
-            if ($sign === '+') {
-                if (array_key_exists('value', $curItemArr) && $curItemArr['value'] === $curItemVal) {
-                    $curItemArr['_sign'] = ' ';
-                } else {
-                    $curItemArr['_signAdd'] = '+';
-                    $curItemArr['valueAdd'] = $curItemVal;
-                }
-            } else {
-                $curItemArr['value'] = $curItemVal;
-            }
-            return $curItemArr;
+            return addItemToCurArr($curItemArr, $curItemVal, $sign);
         }
 
         foreach ($curItem as $itemName => $itemValue) {
-            if (array_key_exists($itemName, $curItemArr)) {
-                $nextItemArr = $curItemArr[$itemName];
-                if (!array_key_exists('value', $nextItemArr) && is_object($itemValue)) {
-                    $nextItemArr['_sign'] = ' ';
-                }
-            } else {
-                $nextItemArr = array('_sign' => $sign);
-            }
+            $nextItemArr = createNextItemArr($itemName, $itemValue, $curItemArr, $sign);
             $curItemArr[$itemName] = $iter($itemValue, $nextItemArr);
         }
-
         return $curItemArr;
     };
     return $iter($item, $itemArr);
+}
+
+function createNextItemArr($itemName, $itemValue, $curItemArr, $sign)
+{
+    if (array_key_exists($itemName, $curItemArr)) {
+        $nextItemArr = $curItemArr[$itemName];
+        if (!array_key_exists('value', $nextItemArr) && is_object($itemValue)) {
+            $nextItemArr['_sign'] = ' ';
+        }
+    } else {
+        $nextItemArr = array('_sign' => $sign, '_signAdd' => '');
+    }
+    return $nextItemArr;
+}
+
+function addItemToCurArr($curItemArr, $curItemVal, $sign)
+{
+    if ($sign === '+') {
+        if (array_key_exists('value', $curItemArr) && $curItemArr['value'] === $curItemVal) {
+            $curItemArr['_sign'] = ' ';
+        } else {
+            $curItemArr['_signAdd'] = '+';
+            $curItemArr['valueAdd'] = $curItemVal;
+        }
+    } else {
+        $curItemArr['value'] = $curItemVal;
+    }
+    return $curItemArr;
 }
 
 function sortDiffArr($diffArr)
@@ -91,55 +101,67 @@ function sortDiffArr($diffArr)
 
 function generateStylishDiff($diffData, $startOffset = -2)
 {
-    $iter = function ($lineName, $curArr, $depth, $parentSign) use (&$iter) {
+    $iter = function ($lineName, $curArr, $depth, $parentSign) use (&$iter, $startOffset) {
         if (!is_array($curArr)) {
             return $curArr . PHP_EOL;
         }
 
-        $object = false;
-        $valueLine = '';
-        $objectLine = '';
-        $valueAddLine = '';
-        if (array_key_exists('_sign', $curArr)) {
-            $sign = $curArr['_sign'];
-        } elseif (array_key_exists('_signAdd', $curArr)) {
-            $sign = $curArr['_signAdd'];
-        } else {
-            $sign = ' ';
-        }
+        $keyNames = ['_sign', '_signAdd', 'value', 'valueAdd'];
+        $sign = getSign($curArr);
         $lineSign = $parentSign === $sign ? ' ' : $sign;
-        if (array_key_exists('value', $curArr)) {
-            $lineVal = $curArr['value'] === '' ? '' : ' ' . $curArr['value'];
-            $valueLine .= str_repeat(' ', $depth) . $lineSign . ' ' . $lineName . ':' . $lineVal . PHP_EOL;
-        }
+        $lineAddSign = $parentSign === $curArr['_signAdd'] ? ' ' : $curArr['_signAdd'];
+        $valueLine = getValueLine($curArr, $lineSign, $lineName, $depth, 'value');
+        $valueAddLine = getValueLine($curArr, $lineAddSign, $lineName, $depth, 'valueAdd');
+
+        $objectLine = '';
+        $object = false;
         foreach ($curArr as $itemName => $itemValue) {
-            if (
-                $itemName === '_sign'
-                || $itemName === '_signAdd'
-                || $itemName === 'value'
-                || $itemName === 'valueAdd'
-            ) {
+            if (in_array($itemName, $keyNames)) {
                 continue;
             }
             $object = true;
             $objectLine .= $iter($itemName, $itemValue, $depth + 4, $sign);
         }
-        if (array_key_exists('valueAdd', $curArr)) {
-            $lineAddSign = $parentSign === $curArr['_signAdd'] ? ' ' : $curArr['_signAdd'];
-            $lineVal = $curArr['valueAdd'] === '' ? '' : ' ' . $curArr['valueAdd'];
-            $valueAddLine .= str_repeat(' ', $depth) . $lineAddSign . ' ' . $lineName . ':' . $lineVal . PHP_EOL;
-        }
-        if ($object && $depth > -2) {
-            $line = str_repeat(' ', $depth) . $lineSign . ' ' . $lineName . ': {' . PHP_EOL;
-            $lineEnd = str_repeat(' ', $depth + 2) . '}' . PHP_EOL;
-        } elseif ($depth > -2) {
-            $line = '';
-            $lineEnd = '';
-        } else {
-            $line = '{' . PHP_EOL;
-            $lineEnd = '}';
-        }
-        return $line . $valueLine . $objectLine . $lineEnd . $valueAddLine;
+        [$lineStart, $lineEnd] = generateLineStartEnd($object, $depth, $lineSign, $lineName, $startOffset);
+        return $lineStart . $valueLine . $objectLine . $lineEnd . $valueAddLine;
     };
     return $iter('', $diffData, $startOffset, '');
+}
+
+function getValueLine($curArr, $lineSign, $lineName, $depth, $valueName)
+{
+    if (array_key_exists($valueName, $curArr)) {
+        $lineVal = $curArr[$valueName] === '' ? '' : ' ' . $curArr[$valueName];
+        $valueLine = str_repeat(' ', $depth) . $lineSign . ' ' . $lineName . ':' . $lineVal . PHP_EOL;
+    } else {
+        $valueLine = '';
+    }
+    return $valueLine;
+}
+
+function getSign($curArr)
+{
+    if (array_key_exists('_sign', $curArr)) {
+        $sign = $curArr['_sign'];
+    } elseif (array_key_exists('_signAdd', $curArr)) {
+        $sign = $curArr['_signAdd'];
+    } else {
+        $sign = ' ';
+    }
+    return $sign;
+}
+
+function generateLineStartEnd($object, $depth, $lineSign, $lineName, $startOffset)
+{
+    if ($object && $depth > $startOffset) {
+        $lineStart = str_repeat(' ', $depth) . $lineSign . ' ' . $lineName . ': {' . PHP_EOL;
+        $lineEnd = str_repeat(' ', $depth + 2) . '}' . PHP_EOL;
+    } elseif ($depth > $startOffset) {
+        $lineStart = '';
+        $lineEnd = '';
+    } else {
+        $lineStart = '{' . PHP_EOL;
+        $lineEnd = '}';
+    }
+    return [$lineStart, $lineEnd];
 }
