8e384eb476d4afb029c59f39400315b0ca052e7b
kemallt <kemallt@gmail.com>
2021-10-29 20:36:26 +0300

    change Differ.php to form diff tree with one thorough

--- src/Differ.php
+++ src/Differ.php
@@ -25,87 +25,105 @@ function genDiff(string $pathToFile1, string $pathToFile2, string $formatter = "
     $data1 = getDataFromFile($pathToFile1);
     $data2 = getDataFromFile($pathToFile2);
 
-    $sortedDiffData = createDiffObjects($data1, $data2);
+    $diffData = convertObject($data1, $data2);
+    $sortedDiffData = sortDiffArr($diffData);
     return getFormattedDiff($sortedDiffData, $formatter);
 }
 
-function createDiffObjects(object $data1, object $data2): array
+function convertObject(object $data1, object $data2): array
 {
-    $convertedData = convertItem($data1, [STATUSNAME => NESTED], DELETED);
-    return convertItem($data2, $convertedData, ADDED);
-}
-
-function convertItem(object $item, array $itemData, string $status): mixed
-{
-    $iter = function ($curItem, $current) use (&$iter, $status) {
-        if (!is_object($curItem)) {
-            $curItemVal = $curItem;
-            return addItemToCurrent($current, $curItemVal, $status);
-        }
-        $currentData = (array)$curItem;
-        $resItem = array_reduce(
-            array_keys($currentData),
-            function ($acc, $itemName) use ($iter, &$currentData, $status): array {
-                $nextItem = getNextItem($itemName, $currentData[$itemName], $acc, $status);
-                $itemVal = $iter($currentData[$itemName], $nextItem);
-                [$inserted, $result] = addNewItemSort($acc, $itemVal, $itemName);
-                return $inserted ? $result : array_merge($result, [$itemName => $itemVal]);
-            },
-            $current
-        );
-        return $resItem;
-    };
-    return $iter($item, $itemData);
-}
-
-function getNextItem(string $itemName, mixed $itemValue, array $current, string $status): array
-{
-    if (array_key_exists($itemName, $current)) {
-        $currentNextItem = $current[$itemName];
-        $itemIsObject = is_object($itemValue);
-        $valueExists = array_key_exists(VALUENAME, $currentNextItem);
-        if ($itemIsObject) {
-            $newStatus = $valueExists ? MODIFIED : NESTED;
-            $nextItem = array_merge($currentNextItem, [STATUSNAME => $newStatus]);
-        } else {
-            $nextItem = $currentNextItem;
-        }
-    } else {
-        $nextItem = array(STATUSNAME => $status);
-    }
-    return $nextItem;
-}
-
-function addItemToCurrent(array $current, mixed $curItemVal, string $status): array
-{
-    if ($status !== ADDED) {
-        return array_merge($current, [VALUENAME => $curItemVal]);
-    }
-    if (array_key_exists(VALUENAME, $current) && $current[VALUENAME] === $curItemVal) {
-        $add = [STATUSNAME => UNCHANGED];
-    } elseif ($current[STATUSNAME] === DELETED) {
-        $add = [STATUSNAME => MODIFIED, NEWVALUENAME => $curItemVal];
-    } else {
-        $add = [STATUSNAME => ADDED, NEWVALUENAME => $curItemVal];
-    }
-    return array_merge($current, $add);
-}
-
-function addNewItemSort(array $acc, mixed $itemVal, string $itemName): array
-{
+    $diffData = processData($data1, $data2);
+    $data2Vals = (array)$data2;
     return array_reduce(
-        array_keys($acc),
-        function ($reduceRound, $key) use ($itemVal, &$acc, $itemName): array {
-            $subAcc = $reduceRound[1];
-            if ($itemName >= $key) {
-                $resSubAcc = array_merge($subAcc, [$key => $acc[$key]]);
-                $inserted = false;
+        array_keys($data2Vals),
+        function ($acc, $itemName) use ($data2) {
+            if (is_object($data2->$itemName)) {
+                $acc[$itemName] = convertObject(new \StdClass(), $data2->$itemName);
             } else {
-                $resSubAcc = array_merge($subAcc, [$itemName => $itemVal, $key => $acc[$key]]);
-                $inserted = true;
+                $acc[$itemName][NEWVALUENAME] = $data2->$itemName;
             }
-            return [$inserted, $resSubAcc];
+            $acc[$itemName]['status'] = ADDED;
+            unset($data2->$itemName);
+            return $acc;
         },
-        [false, []]
+        $diffData
     );
+}
+
+function processData($data1, $data2)
+{
+    $data1Vals = (array)$data1;
+    $data2Vals = (array)$data2 ?? [];
+    return array_reduce(
+        array_keys($data1Vals),
+        function ($acc, $itemName) use ($data1, $data2, $data2Vals) {
+            $acc['status'] = NESTED;
+            if (!in_array($itemName, array_keys($data2Vals))) {
+                return array_merge(
+                    $acc,
+                    [$itemName => createNode($itemName, VALUENAME, DELETED, $data1)]
+                );
+            }
+            if ($data1->$itemName === $data2->$itemName) {
+                return array_merge(
+                    $acc,
+                    [$itemName => createNode($itemName, VALUENAME, UNCHANGED, $data1, $data2)]
+                );
+            }
+            if (!is_object($data1->$itemName) && !is_object($data2->$itemName)) {
+                return array_merge(
+                    $acc,
+                    [$itemName => createNode($itemName, VALUENAME, MODIFIED, $data1, $data2, NEWVALUENAME)]
+                );
+            }
+            if (!is_object($data1->$itemName)) {
+                return array_merge(
+                    $acc,
+                    [$itemName => createNode($itemName, NEWVALUENAME, MODIFIED, $data1, $data2, VALUENAME)]
+                );
+            }
+            if (!is_object($data2->$itemName)) {
+                return array_merge(
+                    $acc,
+                    [$itemName => createNode($itemName, VALUENAME, MODIFIED, $data1, $data2, NEWVALUENAME)]
+                );
+            }
+            $acc[$itemName] = convertObject($data1->$itemName, $data2->$itemName);
+            unset($data1->$itemName);
+            unset($data2->$itemName);
+            return $acc;
+        },
+        []
+    );
+}
+
+function createNode($itemName, $valueName, $status, $data1, $data2 = null, $secValueName = null)
+{
+    $res = array_merge(convertItemValue($data1->$itemName, $valueName), ['status' => $status]);
+    $finRes = ($secValueName === null) ? $res : array_merge(convertItemValue($data2->$itemName, $secValueName), $res);
+    unset($data1->$itemName);
+    if ($data2) {
+        unset($data2->$itemName);
+    }
+    return $finRes;
+}
+
+function convertItemValue($itemValue, $valueName)
+{
+    return (is_object($itemValue)) ? convertObject($itemValue, new \StdClass()) : [$valueName => $itemValue];
+}
+
+function sortDiffArr($diffArr)
+{
+    $iter = function ($curArr) use (&$iter) {
+        if (!is_array($curArr)) {
+            return $curArr;
+        }
+        foreach ($curArr as $itemName => $itemValue) {
+            $curArr[$itemName] = sortDiffArr($itemValue);
+        }
+        ksort($curArr);
+        return $curArr;
+    };
+    return $iter($diffArr);
 }
