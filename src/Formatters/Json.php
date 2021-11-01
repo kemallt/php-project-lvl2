4b0699ed394536e8429b3fea95a9ee3b0267eb40
kemallt <kemallt@gmail.com>
2021-11-01 14:59:32 +0300

    linter fixes 23

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -5,7 +5,7 @@ namespace Differ\Formatters\Json;
 function generateDiff(mixed $diffData): string
 {
     $diffString = json_encode($diffData);
-    if (!$diffString) {
+    if ($diffString === false) {
         throw new \Exception('error with formatting diff to json');
     }
     return $diffString;
